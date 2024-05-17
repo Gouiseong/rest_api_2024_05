9e40389925e179148429704c5579a2c42d454c2c
Gouiseong <rhdmltjd1324@gmail.com>
2024-05-17 13:26:31 +0900

    memebr/me TestCase

---
+++ src/main/java/com/koreait/rest_2024_05/member/dto/MemeberDto.java
@@ -0,0 +1,9 @@
+package com.koreait.rest_2024_05.member.dto;
+
+import java.time.LocalDateTime;
+
+public class MemeberDto {
+    private Long id;
+    private LocalDateTime regDate;
+    private String userName;
+}
